40a3f9a56f5b2528451449afb1b03116e8b569a8
Gift <giftjeremiah001@gmail.com>
2023-08-06 04:24:09 +0100

    chore: moved the config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfig } from './user/typeormconfig';
+import { TypeOrmConfig } from './typeormconfig';
 import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
 
